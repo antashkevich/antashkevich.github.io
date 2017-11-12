0314f29da2b12823f93c9397c6d8e40d9ba04958
Alexandr Antashkevich <antoha_ale6@mail.ru>
2017-11-12 16:53:30 +0300

    1st commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$('.wr-slider-top').slick({
+		dots: true
+	});
+
+    $(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+
+    $("#tabs-menu option").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("data-name");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+
+    /*drop down menu*/
+    $(function(){
+  
+        $('li.dropdown > a').on('click',function(event){        
+            event.preventDefault()            
+            $(this).parent().find('ul').first().toggle(300);            
+            $(this).parent().siblings().find('ul').hide(200);
+            
+            $(this).parent().find('ul').mouseleave(function(){  
+                var thisUI = $(this);
+                $('html').click(function(){
+                    thisUI.hide();
+                    $('html').unbind('click');
+                });
+            });    
+            
+        });      
+    });
+    
+});
